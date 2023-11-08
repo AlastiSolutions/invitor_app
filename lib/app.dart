b1fac2c6a9e9cc0dc230afc807237cbeb541d467
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2023-11-08 06:50:06 -0800

    Update app.dart
    temp testing code for login redirect.

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:invitor_app/main.dart';
 import 'package:invitor_app/theme/theme.dart';
 import 'package:invitor_app/constants/router.dart';
 
@@ -11,10 +12,26 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool currentUserAuthenticated = false;
+
+  @override
+  void initState() {
+    if (supabase.auth.currentUser != null) onAppRestart();
+
+    super.initState();
+  }
+
+  void onAppRestart() {
+    setState(() {
+      currentUserAuthenticated = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      title: 'Invitor',
       theme: theme,
       routerConfig: router,
     );
