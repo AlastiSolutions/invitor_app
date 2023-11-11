cf97359ccc56e84ba5c05eea70028f88e1128478
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2023-11-11 03:59:57 -0800

    Update add_event_screen.dart
    create add event form

--- lib/screens/add_event_screen.dart
+++ lib/screens/add_event_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
 import 'package:invitor_app/main.dart';
@@ -13,12 +14,12 @@ class AddEventScreen extends StatefulWidget {
 }
 
 class _AddCalendarScreenState extends State<AddEventScreen> {
-  // bool _isLoading = false;
-  // bool _isCreating = false;
+  bool _isLoading = false;
+  bool _createdEvent = true;
   bool _showEmail = false;
 
   late final TextEditingController _titleController = TextEditingController();
-  late final TextEditingController? _descriptionController =
+  late final TextEditingController _descriptionController =
       TextEditingController();
   late final TextEditingController _dateController = TextEditingController();
   late final TextEditingController _locationController =
@@ -34,9 +35,13 @@ class _AddCalendarScreenState extends State<AddEventScreen> {
   Future<void> _createEvent() async {
     final title = _titleController.text.trim();
     final location = _locationController.text.trim();
-    final description = _descriptionController!.text.trim();
+    final description = _descriptionController.text.trim();
 
     try {
+      setState(() {
+        _isLoading = true;
+      });
+
       await supabase.from('events').insert({
         'created_at': DateTime.now().toIso8601String(),
         'title': title,
@@ -60,6 +65,10 @@ class _AddCalendarScreenState extends State<AddEventScreen> {
         textColor: Theme.of(context).colorScheme.onError,
         fontSize: 14,
       );
+
+      setState(() {
+        _createdEvent = false;
+      });
     } catch (err) {
       if (!context.mounted) return;
 
@@ -72,73 +81,113 @@ class _AddCalendarScreenState extends State<AddEventScreen> {
         textColor: Theme.of(context).colorScheme.onError,
         fontSize: 14,
       );
+
+      setState(() {
+        _createdEvent = false;
+      });
     } finally {
       _titleController.clear();
       _dateController.clear();
-      _descriptionController!.clear();
+      _descriptionController.clear();
       _locationController.clear();
 
       setState(() {
+        _isLoading = false;
         _showEmail = false;
       });
+    }
+
+    if (!context.mounted) return;
+    if (_createdEvent) {
+      Navigator.pop(context);
+
+      Fluttertoast.showToast(
+        msg: 'Event Created',
+        gravity: ToastGravity.SNACKBAR,
+        toastLength: Toast.LENGTH_LONG,
+        timeInSecForIosWeb: 3,
+        backgroundColor: const Color.fromARGB(255, 93, 240, 101),
+        textColor: Colors.black,
+        fontSize: 14,
+      );
+    } else {
+      Fluttertoast.showToast(
+        msg: 'Event not created. Please try again!',
+        gravity: ToastGravity.TOP,
+        toastLength: Toast.LENGTH_LONG,
+        timeInSecForIosWeb: 4,
+        backgroundColor: Theme.of(context).colorScheme.error,
+        textColor: Theme.of(context).colorScheme.onError,
+        fontSize: 14,
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: const Icon(Icons.close_rounded),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
       body: Container(
         margin: EdgeInsets.symmetric(
             horizontal: MediaQuery.of(context).size.width * .045),
         child: ListView(
           children: [
-            TextFormField(
-              controller: _titleController,
-              decoration: InputDecoration(
-                hintText: 'Event Title',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: _titleController,
+                    decoration: InputDecoration(
+                      hintText: 'Event Title',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            TextFormField(
-              controller: _descriptionController,
-              decoration: InputDecoration(
-                hintText: 'Event Description',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-            TextFormField(
-              controller: _dateController,
-              readOnly: true,
-              decoration: InputDecoration(
-                hintText: 'Event Date',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              onTap: () async {
-                DateTime? pickedDate = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime.now(),
-                  lastDate: DateTime(2100),
-                );
+                SizedBox(width: MediaQuery.of(context).size.width * .0125),
+                Expanded(
+                  child: TextFormField(
+                    controller: _dateController,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      hintText: 'Event Date',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onTap: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime.now(),
+                        lastDate: DateTime(2100),
+                      );
 
-                if (pickedDate != null) {
-                  String formattedDate =
-                      DateFormat('yyyy-MM-dd').format(pickedDate);
+                      if (pickedDate != null) {
+                        String formattedDate =
+                            DateFormat('yyyy-MM-dd').format(pickedDate);
 
-                  setState(() {
-                    _date = pickedDate;
-                    _dateController.text = formattedDate;
-                  });
-                }
-              },
+                        setState(() {
+                          _date = pickedDate;
+                          _dateController.text = formattedDate;
+                        });
+                      }
+                    },
+                  ),
+                ),
+              ],
             ),
+            SizedBox(height: MediaQuery.of(context).size.height * .025),
             TextFormField(
               controller: _locationController,
               decoration: InputDecoration(
@@ -148,17 +197,82 @@ class _AddCalendarScreenState extends State<AddEventScreen> {
                 ),
               ),
             ),
-            Switch(
-              value: _showEmail,
-              onChanged: (val) {
-                setState(() {
-                  _showEmail = val;
-                });
-              },
+            SizedBox(height: MediaQuery.of(context).size.height * .025),
+            TextFormField(
+              expands: false,
+              minLines: 1,
+              maxLines: 3,
+              textAlign: TextAlign.start,
+              textAlignVertical: TextAlignVertical.top,
+              controller: _descriptionController,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * .025,
+                  right: MediaQuery.of(context).size.width * .025,
+                  bottom: MediaQuery.of(context).size.height * .1,
+                  top: MediaQuery.of(context).size.height * .025,
+                ),
+                hintText: 'Event Description',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
             ),
-            ElevatedButton(
-              onPressed: _createEvent,
-              child: const Text('Create Event'),
+            SizedBox(height: MediaQuery.of(context).size.height * .0125),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Show Email on Event",
+                  style: GoogleFonts.lato(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Switch(
+                  value: _showEmail,
+                  onChanged: (val) {
+                    setState(() {
+                      _showEmail = val;
+                    });
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * .0125),
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * .20,
+              ),
+              child: ElevatedButton(
+                onPressed: _createEvent,
+                style: ButtonStyle(
+                  fixedSize: MaterialStatePropertyAll(
+                    Size(
+                      MediaQuery.of(context).size.width * .10,
+                      MediaQuery.of(context).size.height * .05,
+                    ),
+                  ),
+                  backgroundColor: MaterialStatePropertyAll(
+                    Theme.of(context).colorScheme.primaryContainer,
+                  ),
+                  foregroundColor: MaterialStatePropertyAll(
+                    Theme.of(context).colorScheme.onPrimaryContainer,
+                  ),
+                  shape: MaterialStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      side: BorderSide(
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                  ),
+                ),
+                child: _isLoading
+                    ? const CircularProgressIndicator()
+                    : const Text('Create Event'),
+              ),
             ),
           ],
         ),
