14d34ec0cd7c6984a836b882b06d122c18de99be
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2023-11-11 17:09:02 -0800

    Update calendar_screen.dart
    temp calendar screen

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:invitor_app/widgets/global/bottom_nav_bar.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
@@ -9,10 +10,49 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode.toggledOff;
+
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  DateTime? _rangeStart;
+  DateTime? _rangeEnd;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedDay = _focusedDay;
+    // _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
+  }
+
+  @override
+  void dispose() {
+    // _selectedEvents.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: BottomNavBar(index: 1),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+      ),
+      bottomNavigationBar: const BottomNavBar(index: 1),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * .1,
+          vertical: MediaQuery.of(context).size.height * .0125,
+        ),
+        child: TableCalendar(
+          locale: 'en_US',
+          focusedDay: DateTime.now(),
+          firstDay: DateTime.now(),
+          lastDay: DateTime(2100),
+        ),
+      ),
     );
   }
 }
